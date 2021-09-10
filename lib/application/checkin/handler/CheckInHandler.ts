156ec8c6e585fc4b97b45dcd5bac5d474b5a5ac3
potados99 <potados99@gmail.com>
2021-09-11 03:21:26 +0900

    chore: 문서 업데이트

--- lib/application/checkin/handler/CheckInHandler.ts
+++ lib/application/checkin/handler/CheckInHandler.ts
@@ -1,5 +1,6 @@
-import {Booking, CheckIn, VisitRecord} from '@inu-cafeteria/backend-core';
+import {Booking, CheckIn, logger, VisitRecord} from '@inu-cafeteria/backend-core';
 import CheckInValidator from '../validation/CheckInValidator';
+import {stringifyError} from '../../../common/utils/error';
 
 export default class CheckInHandler {
   constructor(private readonly booking: Booking) {}
@@ -31,9 +32,15 @@ export default class CheckInHandler {
   }
 
   private async checkIn(booking: Booking) {
-    await CheckIn.create({
-      bookingId: booking.id,
-      checkedInAt: new Date(),
-    }).save();
+    try {
+      await CheckIn.create({
+        bookingId: booking.id,
+        checkedInAt: new Date(),
+      }).save();
+    } catch (e) {
+      // 검증을 거치지 않고 이곳에 도달할 수 있습니다. 예를 들어 룰이 꺼져있을 수 있습니다.
+      // 해당 경우는 성공이라고 보아야 하기에, 에러를 올려보내지 않고 조용히 처리합니다.
+      logger.info(`체크인 저장이 실패하는 일이 발생했습니다: ${stringifyError(e)}`);
+    }
   }
 }
