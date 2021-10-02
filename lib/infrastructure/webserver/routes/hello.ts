6f09b1dc0591b730c48815b17cba67f6c23f68b3
potados99 <potados99@gmail.com>
2021-10-03 02:13:02 +0900

    chore: 루트 헬로 메시지 이뿌게

--- lib/infrastructure/webserver/routes/hello.ts
+++ lib/infrastructure/webserver/routes/hello.ts
@@ -1,8 +1,14 @@
+import packageInfo from '../../../../package.json';
 import {defineRoute} from '../libs/route';
 import {defineSchema} from '../libs/schema';
 
 const schema = defineSchema({});
 
 export default defineRoute('get', '/', schema, async (req, res) => {
-  res.send('오이! 여긴 어떻게 알았냐구!');
+  res.send(
+    `카페테리아 콘솔 API 서버 v${packageInfo.version} 
+    / 서버시각 ${new Date().toLocaleString()} 
+    / 비상연락 010-2922-2661(앱센터 송병준) 
+    / 오이! 여긴 어떻게 알았냐구!`
+  );
 });
