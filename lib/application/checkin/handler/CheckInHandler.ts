a176bbe9f12546b1820a63d7bce15c2b68f01002
potados99 <potados99@gmail.com>
2021-10-11 03:41:43 +0900

    feat: 체크인 기록 중복으로 남기지 않음

--- lib/application/checkin/handler/CheckInHandler.ts
+++ lib/application/checkin/handler/CheckInHandler.ts
@@ -1,6 +1,8 @@
 import {stringifyError} from '../../../common/utils/error';
 import CheckInValidator from '../validation/CheckInValidator';
 import {Booking, CheckIn, logger, VisitRecord} from '@inu-cafeteria/backend-core';
+import {subMinutes} from 'date-fns';
+import {MoreThan} from 'typeorm';
 
 export default class CheckInHandler {
   constructor(private readonly booking: Booking) {}
@@ -19,7 +21,7 @@ export default class CheckInHandler {
       ? await validator.validateGracefully()
       : await validator.validateStrictly();
 
-    await this.leaveVisitRecord(booking);
+    await this.leaveVisitRecordIfNotDuplicated(booking);
 
     if (error) {
       throw error;
@@ -28,14 +30,27 @@ export default class CheckInHandler {
     await this.checkIn(booking);
   }
 
-  private async leaveVisitRecord(booking: Booking) {
-    await VisitRecord.create({
+  private async leaveVisitRecordIfNotDuplicated(booking: Booking) {
+    const existingInLastMinute = await VisitRecord.findOne({
       bookingId: booking.id,
-      studentId: booking.user.studentId,
-      phoneNumber: booking.user.phoneNumber,
-      cafeteriaId: booking.cafeteriaId,
-      visitedAt: new Date(),
-    }).save();
+      visitedAt: MoreThan(subMinutes(new Date(), 1)),
+    });
+
+    if (existingInLastMinute == null) {
+      logger.info(`입장 기록을 남깁니다.`);
+
+      await VisitRecord.create({
+        bookingId: booking.id,
+        studentId: booking.user.studentId,
+        phoneNumber: booking.user.phoneNumber,
+        cafeteriaId: booking.cafeteriaId,
+        visitedAt: new Date(),
+      }).save();
+    } else {
+      logger.warn(
+        `이 사용자는 1분 안에 해당 바코드로 체크인을 시도한 기록이 있습니다. 중복으로 기록을 남기지 않습니다.`
+      );
+    }
   }
 
   private async checkIn(booking: Booking) {
@@ -47,7 +62,7 @@ export default class CheckInHandler {
     } catch (e) {
       // 검증을 거치지 않고 이곳에 도달할 수 있습니다. 예를 들어, 중복 방지 룰이 꺼져있을 수 있습니다.
       // 해당 경우는 성공이라고 보아야 하기에, 에러를 올려보내지 않고 조용히 처리합니다.
-      logger.info(`체크인 저장이 실패하는 일이 발생했습니다: ${stringifyError(e)}`);
+      logger.info(`체크인 저장에 실패하는 일이 발생했습니다: ${stringifyError(e)}`);
     }
   }
 }
