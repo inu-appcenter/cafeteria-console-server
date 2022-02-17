6923ae54ffe2f7d0881a4485dcb395f82d7b19da
potados99 <potados99@gmail.com>
2022-02-17 23:17:25 +0900

    refactor: GetCheckInContextParams 규격 변경

--- lib/application/checkin/GetCheckInContext.ts
+++ lib/application/checkin/GetCheckInContext.ts
@@ -21,12 +21,12 @@ import UseCase from '../../core/base/UseCase';
 import Context from './Context';
 
 export type GetCheckInContextParams = {
-  cafeteriaId?: number;
+  cafeteriaId: number;
 };
 
 class GetCheckInContext extends UseCase<GetCheckInContextParams, Context> {
   async onExecute({cafeteriaId}: GetCheckInContextParams): Promise<Context> {
-    return await Context.forNow(cafeteriaId || 1 /*TODO*/, new Date(/* '2021-08-23 08:31:00' */));
+    return await Context.forNow(cafeteriaId, new Date(/* '2021-08-23 08:31:00' */));
   }
 }
 
