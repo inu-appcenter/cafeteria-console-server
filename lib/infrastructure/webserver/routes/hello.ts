53d430b2e32c37dfcb0859507425afa93de2959c
potados99 <potados99@gmail.com>
2022-09-13 12:39:47 +0900

    feat: 관리자 연락처 iframe 추가

--- lib/infrastructure/webserver/routes/hello.ts
+++ lib/infrastructure/webserver/routes/hello.ts
@@ -24,9 +24,9 @@ const schema = defineSchema({});
 
 export default defineRoute('get', '/', schema, async (req, res) => {
   return res.send(
-    `카페테리아 콘솔 API 서버 v${packageInfo.version} 
+    `<iframe src='https://contacts.inuappcenter.kr?service=cafeteria' style='border: none;'></iframe><br><br>
+    카페테리아 콘솔 API 서버 v${packageInfo.version} 
     / 서버시각 ${new Date().toLocaleString()} 
-    / 비상연락 010-2922-2661(앱센터 송병준) 
     / 오이! 여긴 어떻게 알았냐구!`
   );
 });
